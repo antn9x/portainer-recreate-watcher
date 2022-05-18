9ecd35e5becfa1eb1cea38b21c0f48497aac5075
antn <antn@taap.vn>
2022-05-18 15:42:12 +0700

    use fastify server factory

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,11 @@ FROM node:16.13.1-alpine
 
 # node app port
 EXPOSE 3000
-
-COPY build ./app/src
-COPY docs ./app/docs
-
 WORKDIR /app/
+
+COPY src ./app/src
+COPY index.js ./app/index.js
+
 
 COPY package.json process.yml ./
 # RUN ls -alh
@@ -16,9 +16,5 @@ COPY package.json process.yml ./
 
 ENV NODE_ENV=staging
 ENV PORT=3000
-ENV REDIS_URI=redis://host.docker.internal:6379
-ENV JWT_SECRET=firjfufguewitghidsf
-ENV DATABASE_URI=mongodb://host.docker.internal:27017/meta-cana
-ENV TELEGRAM_ISLOG=false
 
 CMD yarn deploy-cluster
--- index.js
+++ index.js
@@ -1,14 +1,73 @@
-const uWebSocket = require('uWebSockets.js');
+const server = require('./src/server.js')({})
+const axios = require("axios");
 
-const server = uWebSocket.App()
-server.any('/*', (res, req) => {
-  res.writeStatus('200 OK')
-    .writeHeader('IsExample', 'Yes')
-    .end('Hello there!');
-});
+async function recreate(connectUri, stackId, imageUri) {
+  const [username, password, host] = connectUri.split('@');
+  axios.defaults.baseURL = host;
+  axios.defaults.headers.post['Content-Type'] = 'application/json';
+  const auth = await axios.post('/auth', { username, password })
+  // console.log(auth.data)
+  axios.defaults.headers.common.Authorization = `Bearer ${auth.data.jwt}`;
 
-server.listen(process.env.PORT || 3000, '0.0.0.0', (token) => {
-  if (token) {
-    console.log('token on port ', token);
+  const registries = await axios.get(`/endpoints/${stackId}/registries`)
+  const ghRegistry = registries.data.find((reg) => reg.URL.includes('ghcr.io'))
+  const listContainers = await axios.get(`/endpoints/${stackId}/docker/containers/json?all=1`)
+  // console.log(listContainers.data)
+  const selected = listContainers.data.find((container) => imageUri.includes(container.Image));
+  const containerName = selected.Names[0].slice(1);
+  // console.log(containerName)
+  const tokenData = {
+    registryId: ghRegistry.Id
   }
+  const ghToken = Buffer.from(JSON.stringify(tokenData)).toString('base64');
+  // console.log('ghToken', ghRegistry, ghToken)
+  const create = await axios.post(`/endpoints/${stackId}/docker/images/create?fromImage=${encodeURIComponent(imageUri)}`, {}, {
+    headers: {
+      'x-registry-auth': ghToken
+    }
+  })
+  // console.log('create', create.data)
+  if (selected.State === 'running') {
+    const stop = await axios.post(`/endpoints/${stackId}/docker/containers/${selected.Id}/stop`)
+    console.log('stop', stop.data)
+  }
+  const rename = await axios.post(`/endpoints/${stackId}/docker/containers/${selected.Id}/rename?name=${containerName}-old`)
+  console.log('rename', rename.data)
+  const container = await axios.post(`/endpoints/${stackId}/docker/containers/create?name=${containerName}`, selected)
+  console.log('container', container.data)
+  try {
+    const connect = await axios.post(`/endpoints/${stackId}/docker/networks/${Object.keys(selected.NetworkSettings.Networks)[0]}/connect`)
+    console.log('connect', connect.data)
+  } catch (error) {
+    console.log('error', error.message)
+  }
+  const start = await axios.post(`/endpoints/${stackId}/docker/containers/${container.data.Id}/start`)
+  console.log('start', start.data)
+  const control = await axios.put(`/resource_controls/${container.data.Portainer.ResourceControl.Id}`, { "AdministratorsOnly": true, Public: false })
+  console.log('control', control.data)
+  const deleteContainer = await axios.delete(`/endpoints/${stackId}/docker/containers/${selected.Id}?force=true&v=1`)
+  console.log('deleteContainer', deleteContainer.data)
+}
+
+const serverFactory = (handler, opts) => {
+  server.on('request', handler)
+  return server;
+}
+
+const fastify = require("fastify");
+const app = fastify({
+  logger: true,
+  serverFactory
 });
+
+app.post('/', (req, reply) => {
+  const { connectUri, stackId, imageUri } = req.body;
+  recreate(connectUri, stackId, imageUri);
+  reply.status(200).send('OK\n');
+})
+
+app.ready().then(() => {
+  app.listen(process.env.PORT || 3000, '0.0.0.0', (sock) => { // Note: you cannot do server.listen, as Fastify apparently needs to set some things
+    if (sock) console.log('listening')
+  })
+})
